9fcb7791842f5df2d866502611593d386677b687
Eric Zheng <zheng.eric.y@gmail.com>
2019-07-30 08:22:54 -0400

    increase resolution

--- readme2tex.sh
+++ readme2tex.sh
@@ -1,6 +1,6 @@
 # the regular readme2tex stuff doesn't seem to work that robustly
 for file in svgs/*.svg; do
-  convert -density 72 $file "${file%.svg}.png"
+  convert -density 300 $file "${file%.svg}.png"
 done
 python3 -m readme2tex --output README.md INPUT.md --nocdn --branch master --pngtrick
 
