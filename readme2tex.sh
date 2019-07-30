3fa215116224328853b1119a25fdfbe3029406e2
Eric Zheng <zheng.eric.y@gmail.com>
2019-07-30 08:17:57 -0400

    aiya

--- readme2tex.sh
+++ readme2tex.sh
@@ -1,3 +1,6 @@
 # the regular readme2tex stuff doesn't seem to work that robustly
+for file in svgs/*.svg; do
+  convert $file "${file%.svg}.png"
+done
 python3 -m readme2tex --output README.md INPUT.md --nocdn --branch master --pngtrick
 
